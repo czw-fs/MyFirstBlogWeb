916626e5720a47697f42257b86a3d35ccd00a2e7
fs <fs@qq.com>
2023-10-13 14:59:51 +0800

    注册 done

--- fs-framework/src/main/java/org/example/framework/service/impl/UserDetailsServiceImpl.java
+++ fs-framework/src/main/java/org/example/framework/service/impl/UserDetailsServiceImpl.java
@@ -3,8 +3,10 @@ package org.example.framework.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.example.framework.domain.AppHttpCodeEnum;
 import org.example.framework.domain.LoginUser;
 import org.example.framework.domain.ResponseResult;
+import org.example.framework.domain.SystemException;
 import org.example.framework.domain.entity.User;
 import org.example.framework.mapper.UserMapper;
 import org.example.framework.service.UserService;
@@ -15,7 +17,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.Objects;
 
@@ -24,6 +28,9 @@ public class UserDetailsServiceImpl extends ServiceImpl<UserMapper,User> impleme
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -57,6 +64,38 @@ public class UserDetailsServiceImpl extends ServiceImpl<UserMapper,User> impleme
     @Override
     public ResponseResult updateUserInfo(User user) {
         updateById(user);
+        return ResponseResult.okResult();
+    }
+
+
+    @Override
+    public ResponseResult register(User user) {
+        //对数据进行非空判断
+        if(!StringUtils.hasText(user.getUserName())){
+            throw new SystemException(AppHttpCodeEnum.USERNAME_NOT_NULL);
+        }
+        if(!StringUtils.hasText(user.getPassword())){
+            throw new SystemException(AppHttpCodeEnum.PASSWORD_NOT_NULL);
+        }
+        if(!StringUtils.hasText(user.getEmail())){
+            throw new SystemException(AppHttpCodeEnum.EMAIL_NOT_NULL);
+        }
+        if(!StringUtils.hasText(user.getNickName())){
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_NOT_NULL);
+        }
+//        //对数据进行是否存在的判断
+//        if(userNameExist(user.getUserName())){
+//            throw new SystemException(AppHttpCodeEnum.USERNAME_EXIST);
+//        }
+//        if(nickNameExist(user.getNickName())){
+//            throw new SystemException(AppHttpCodeEnum.NICKNAME_EXIST);
+//        }
+        //...
+        //对密码进行加密
+        String encodePassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodePassword);
+        //存入数据库
+        save(user);
         return ResponseResult.okResult();
     }
 }
